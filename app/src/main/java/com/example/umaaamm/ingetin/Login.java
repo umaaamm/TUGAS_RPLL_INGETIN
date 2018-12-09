082b5468f24b222f568c473eb54a678bf65bf417
Umam <umam.tekno@gmail.com>
2018-12-09 22:27:21 +0700

    Add Login xml

--- app/src/main/java/com/example/umaaamm/ingetin/Login.java
+++ app/src/main/java/com/example/umaaamm/ingetin/Login.java
@@ -1,4 +1,14 @@
 package com.example.umaaamm.ingetin;
 
-public class Login  {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+    }
+
 }
